8fa0c7e3c19db24c7c4f6bc5d6549593d59d5a12
qiangmoumou <1092157587@qq.com>
2019-09-06 17:50:51 +0800

    vue脚手架使用教程

---
+++ 02-cli3.my-testvuecli3/src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
+
+// $mount('#app') 和 el: '#app'    // 一个意思,做了个判断而已
